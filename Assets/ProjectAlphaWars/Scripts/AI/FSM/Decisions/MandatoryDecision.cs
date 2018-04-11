902b490eb504e50d652a9a719790be3ce8da33d1
Calebe Santos <calebe.bsantos@outlook.com>
2018-04-11 14:05:35 -0300

    :boom: Change editor settings

---
+++ Assets/ProjectAlphaWars/Scripts/AI/FSM/Decisions/MandatoryDecision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/Attack")]
+public class MandatoryDecision : Decision
+{
+    //Decisão para atacar
+    //Dentro do range específico do tipo do personagem
+    public override bool Decide(StateController stateController)
+    {
+        return Mandatory(stateController);
+    }
+
+    private bool Mandatory(StateController stateController)
+    {
+        return stateController.chaseTarget == null;
+    }
+}
